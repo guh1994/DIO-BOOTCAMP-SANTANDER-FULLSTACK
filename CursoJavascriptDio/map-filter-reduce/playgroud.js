b731f4c1262ccd27c56a2306203715de35c541e5
Gustavo Silva <gustavo.ferreira.silva1994@hotmail.com>
2022-07-27 16:55:48 -0300

    Reduce

--- CursoJavascriptDio/map-filter-reduce/playgroud.js
+++ CursoJavascriptDio/map-filter-reduce/playgroud.js
@@ -22,11 +22,46 @@ function filtraPares(arr){
 function callback(item){
     return item % 2 === 0;
 }
+function somaNumero(arr){
+    return arr.reduce(function(prev, current){
+        console.log({prev});
+        console.log({current});
+        return prev + current;
+    },0);
+}
+
+function calculaSaldo(saldoDisponivel, lista){
+    return lista.reduce(function(prev, current, index){
+        console.log('rodada', index + 1);
+        console.log({prev});
+        console.log({current})
+        return prev - current.preco;
+    }, saldoDisponivel)
+}
+
 const nums = [1,2];
 const nums2 = [2,4,6,8,10];
 const nums3 = [1,23,55,67,30,2,4];
+const arr = [1,2];
+const lista = [
+    {
+        name: 'Sabao em po',
+        preco: 30,
+    },
+    {
+        name: 'cereal',
+        preco: 12,
+    },
+    {
+        name: 'toalha',
+        preco: 30,
+    },
+];
+const saldoDisponivel = 100;
 
 console.log('this -> maçã ',mapComThis(nums,maca));
 console.log('this -> laranja ',mapComThis(nums,laranja));
 console.log(mapSemThis(nums2));
 console.log(filtraPares(nums3));
+console.log(somaNumero(arr));
+console.log(calculaSaldo(saldoDisponivel,lista));
